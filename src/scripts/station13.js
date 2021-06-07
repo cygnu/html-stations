fb38b09d23b162d0c44856dd43103b45e88d2aa7
cygnu <crucia8@icloud.com>
2021-06-08 04:17:28 +0900

    solved station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function getData() {
     // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+    let printWord = () => {
+        console.log("Arrrow");
+    };
+
+    return printWord(); // ここにアロー関数の処理を書く
 }
